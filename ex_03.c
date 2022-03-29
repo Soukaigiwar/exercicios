608a1856048532a1519edfc1e160ef21ba53ae89
Soukaigiwar <54086765+Soukaigiwar@users.noreply.github.com>
2022-03-29 18:29:35 -0300

    Update ex_03.c

--- ex_03.c
+++ ex_03.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main() {
+
+  int read_value;
+  
+  read_value = 0;
+  
+  printf("Digite qualquer número inteiro: ");
+  scanf("%i", &read_value);
+  printf("\nO valor digitado em Hexadecimal é: %x.", read_value);
+  printf("\nO valor digitado em Octadecimal é: %o.", read_value);
+  return 0;
+}
