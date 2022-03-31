b98b90f3d2c143badb19308eb49b0501b493514c
Soukaigiwar <54086765+Soukaigiwar@users.noreply.github.com>
2022-03-31 00:12:38 -0300

    ex_01 from group 2 incomplete

---
+++ group_3/ex_01.c
@@ -0,0 +1,7 @@
+// Escrever a seqüência de comandos do pré-processador que define o tipo de
+// dados BOOL (com as constantes TRUE e FALSE) caso isso ainda não tenha
+// sido feito.
+
+// #ifndef BOOL 
+// #define true
+// #endif
