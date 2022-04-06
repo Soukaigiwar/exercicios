ed8f05d31a16c79869a6102f2920e1ef994f1d9f
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2022-04-06 10:16:18 -0300

    Update ex_04.c

--- group_4/ex_04.c
+++ group_4/ex_04.c
@@ -1,60 +1,64 @@
 #include <stdio.h>
 
-void    options(int total)
+void	option_1(float total)
 {
-	int option;
+	total *= 0.9;
+	printf("\nValor total com essa opção foi R$ %.2f", total);
+}
+
+void	option_2(float total)
+{
+	total *= 0.5;
+	printf("\nPague em 2x de R$ %.2f", total);
+}
+
+void	option_3(float total, int installments)
+{
+	total = (total / installments) * (1 + (0.03 * installments));
+	printf("\nCom essa opção, voce paga\n");
+	printf("%d parcelas de R$ %.2f ", installments, total);
+	printf("que da o total de R$ %.2f\n", installments * total);
+}
+
+void	options(int total)
+{
+	int	option;
 	int	installments;
-    printf("\n\t\t\t\t01 - A vista com 10% de desconto.");
-    printf("\n\t\t\t\t02 - Duas vezes com o preço da etiqueta.");
-    printf("\n\t\t\t\t03 - De 3 a 10 vezes com juros de 3% ao mês");
-    printf("\n\t\t\t\t\tSomente para compras acima de R$ 100,00");
-    scanf("%d", &option);
-    switch (option)
-    {
-        case 1:
-            option_1(total);
-            break;
-        case 2:
-            option_2(total);
-            break;
-        case 3:
-            printf("Digite quantas parcelas de 3 até 10:");
-            scanf("%d", &installments);
-            if (installments > 2 && installments < 11)
-                option_3(total, installments);
-                break;
-        default:
-            break;
-    }
+
+	printf("\n\t\t1 - A vista com 10 porcento de desconto.");
+	printf("\n\t\t2 - Duas vezes com o preço da etiqueta.");
+	printf("\n\t\t3 - De 3 a 10 vezes com juros de 3 porcento ao mês");
+	printf("\n\t\t    Somente para compras acima de R$ 100,00\n");
+	scanf("%d", &option);
+	switch (option)
+	{
+		case 1:
+			option_1(total);
+			break;
+		case 2:
+			option_2(total);
+			break;
+		case 3:
+			printf("\nDigite quantas parcelas de 3 até 10:\n");
+			scanf("%d", &installments);
+			if (installments > 2 && installments < 11)
+				option_3(total, installments);
+				break;
+		default:
+			break;
+	}
 }
 
-void    option_1(float total)
-{
-    total *= 0.9;
-    printf("\nValor total com essa opção foi: %.2f", total);
-}
 
-void    option_2(float total)
+int	main(void)
 {
-    total *= 0.5;
-    printf("\nPague em 2x de R$: %.2f", total);
-}
+	float	total;
+	int		option;
 
-void    option_3(float total, int installments)
-{
-    total = (total / installments) * (1 + (0.03 * installments);
-    printf("\nValor total com essa opção foi: %.2f", total);
-}
-
-int main(void)
-{
-    float   total;
-    int     option;
-
-    printf("Digite o total gasto:");
-    scanf("%.2f", &total);
-    printf("\nForma de pagamento:\t");
-    options(total);
-    printf("\n\n");
-    return (0);
+	printf("\nDigite o total gasto R$ ");
+	scanf("%f", &total);
+	printf("\nForma de pagamento:\t");
+	options(total);
+	printf("\n\n");
+	return (0);
 }
