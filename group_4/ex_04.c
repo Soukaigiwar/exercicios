148a9bd39401d4a56637dddbc86f29db04cf9958
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2022-04-05 23:44:32 -0300

    option 3

--- group_4/ex_04.c
+++ group_4/ex_04.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
 
+void    option_1(float total)
+{
+    total *= 0.9;
+    printf("\nValor total com essa opção foi: %.2f", total);
+}
+
+void    option_2(float total)
+{
+    total *= 0.5;
+    printf("\nPague em 2x de R$: %.2f", total);
+}
+
+void    option_3(float total, int installments)
+{
+    total = (total / installments) * (1 + (0.03 * installments);
+    printf("\nValor total com essa opção foi: %.2f", total);
+}
+
 int main(void)
 {
     float   total;
     int     option;
-    char    pay_method[3];
+    int     installments;
 
     printf("Digite o total gasto:");
     scanf("%.2f", &total);
@@ -14,5 +32,23 @@ int main(void)
     printf("\n\t\t\t\t03 - De 3 a 10 vezes com juros de 3% ao mês");
     printf("\n\t\t\t\t\tSomente para compras acima de R$ 100,00");
     scanf("%d", &option);
+    switch (option)
+    {
+        case 1:
+            option_1(total);
+            break;
+        case 2:
+            option_2(total);
+            break;
+        case 3:
+            printf("Digite quantas parcelas de 3 até 10:");
+            scanf("%d", &installments);
+            if (installments > 2 && installments < 11)
+                option_3(total, installments);
+                break;
+        default:
+            break;
+    }
+    printf("\n\n");
     return (0);
 }
