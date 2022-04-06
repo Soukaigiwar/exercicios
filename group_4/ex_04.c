000735fb6b97bf06ccb51a81b7787613ad015cb0
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2022-04-05 23:47:51 -0300

    added options function

--- group_4/ex_04.c
+++ group_4/ex_04.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
 
+void    options(void)
+{
+    printf("\n\t\t\t\t01 - A vista com 10% de desconto.");
+    printf("\n\t\t\t\t02 - Duas vezes com o preço da etiqueta.");
+    printf("\n\t\t\t\t03 - De 3 a 10 vezes com juros de 3% ao mês");
+    printf("\n\t\t\t\t\tSomente para compras acima de R$ 100,00");
+    scanf("%d", &option);
+    switch (option)
+    {
+        case 1:
+            option_1(total);
+            break;
+        case 2:
+            option_2(total);
+            break;
+        case 3:
+            printf("Digite quantas parcelas de 3 até 10:");
+            scanf("%d", &installments);
+            if (installments > 2 && installments < 11)
+                option_3(total, installments);
+                break;
+        default:
+            break;
+    }
+}
+
 void    option_1(float total)
 {
     total *= 0.9;
@@ -27,28 +53,7 @@ int main(void)
     printf("Digite o total gasto:");
     scanf("%.2f", &total);
     printf("\nForma de pagamento:\t");
-    printf("\n\t\t\t\t01 - A vista com 10% de desconto.");
-    printf("\n\t\t\t\t02 - Duas vezes com o preço da etiqueta.");
-    printf("\n\t\t\t\t03 - De 3 a 10 vezes com juros de 3% ao mês");
-    printf("\n\t\t\t\t\tSomente para compras acima de R$ 100,00");
-    scanf("%d", &option);
-    switch (option)
-    {
-        case 1:
-            option_1(total);
-            break;
-        case 2:
-            option_2(total);
-            break;
-        case 3:
-            printf("Digite quantas parcelas de 3 até 10:");
-            scanf("%d", &installments);
-            if (installments > 2 && installments < 11)
-                option_3(total, installments);
-                break;
-        default:
-            break;
-    }
+    options();
     printf("\n\n");
     return (0);
 }
